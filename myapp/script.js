1a6ca3fd0dbe7ab03dbf285feb43a7f9332449c2
RicardoSTU <147540745+RicardoSTU@users.noreply.github.com>
2024-01-30 22:37:32 +0600

    Add files via upload

---
+++ myapp/script.js
@@ -0,0 +1,13 @@
+// script.js
+
+function login() {
+    // Assuming a successful login for demonstration purposes
+    // You may need to add actual authentication logic here
+
+    // Redirect to another page (replace 'newPage.html' with your desired page)
+    window.location.href = 'info.html';
+
+    // Prevent the form from submitting (since we're handling the redirection via JavaScript)
+    return false;
+}
+
